5e454af77ec13cf0e128cd99f15453e9b77f5383
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2020-09-29 12:05:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,10 @@ var canvas
 var car1i, car2i
 
 function preload(){
-car1i=loadImage("images//car1.png")
-car2i=loadImage("images//car2.png")
-car3i=loadImage("images//car3.png")
-car4i=loadImage("images//car4.png")
+car1i=loadImage("images/car1.png")
+car2i=loadImage("images/car2.png")
+car3i=loadImage("images/car3.png")
+car4i=loadImage("images/car4.png")
 
 }
 
