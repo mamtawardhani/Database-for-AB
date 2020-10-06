f47a8a937e5201aba50abb4f28b6859bc714d686
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2020-10-06 12:28:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,18 +7,21 @@ var allplayers;
 var currentindex=0
 var index,x
 var canvas
-var car1i, car2i
+var car1i, car2i, car3i, car4i
+var bg
+var title
 
 function preload(){
 car1i=loadImage("images/car1.png")
 car2i=loadImage("images/car2.png")
 car3i=loadImage("images/car3.png")
 car4i=loadImage("images/car4.png")
+bg=loadImage("images/track.jpg")
 
 }
 
 function setup(){
-    canvas=createCanvas(displayWidth-20,displayHeight-30)
+    canvas=createCanvas(displayWidth-10,displayHeight-145)
     db=firebase.database()
 
     //var dbRef=db.ref("gamestate")
@@ -55,16 +58,24 @@ function setup(){
     car2=createSprite(200,200,30,30)
     car2.shapeColor="blue"
     car2.addImage("c2",car2i)
-    cars=[car1,car2]
+
+    car3=createSprite(200,200,30,30)
+    car3.shapeColor="purple"
+    car3.addImage("c3",car3i)
+    car4=createSprite(200,200,30,30)
+    car4.shapeColor="white"
+    car4.addImage("c4",car4i)
+    cars=[car1,car2,car3,car4]
 }
 
 function draw(){
 
 
-    if(pc==2 && gs==0){
+    if(pc==4 && gs==0){
         db.ref('/').update({
             gamestate:1
         })
+        
     }
 
     if(allplayers==undefined && gs==1){
@@ -72,26 +83,50 @@ function draw(){
         db.ref('players').on("value",function(data){
             allplayers=data.val()
         })
-
+      
     }
 
     if(gs==1){
-        background(0)
+     background(bg)
+    //  image(bg,0,0,displayWidth,displayHeight)
+       
+        
         var index=0
-        var x=250
+        var x=400
         for(var i in allplayers){
             cars[index].x=x
             x=x+250
             cars[index].y=allplayers[i].y
+
+            if(index==currentindex-1){
+                stroke(10)
+                fill("red")
+                ellipse(cars[currentindex-1].x,cars[currentindex-1].y,60,60)
+                camera.position.y=cars[index].y
+                camera.position.x=displayWidth/2
+            }
             index+=1
         }
 
         if(keyDown("up")){
             cars[currentindex-1].y-=5
 
+            
+
             db.ref("players/player"+(currentindex)).update({
                 y:cars[currentindex-1].y
             })
+        }
+
+        if(cars[currentindex-1].y<-3860){
+            gs=2
+        }
+
+        if(gs===2){
+            
+          var end=createElement("h3")
+          end.html("Game Over!")
+          end.position(displayWidth/2,displayHeight-100)
         }
         drawSprites()
     }
